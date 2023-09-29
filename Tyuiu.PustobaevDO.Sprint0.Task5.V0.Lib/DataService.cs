af1598a8ed4c849731db31cb8bace05475820a04
Tyuiu.PustobaevDO@outlook.com <Tyuiu.PustobaevDO@outlook.com>
2023-09-29 14:26:19 +0500

    Task5

---
+++ Tyuiu.PustobaevDO.Sprint0.Task5.V0.Lib/DataService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.PustobaevDO.Sprint0.Task5.V0.Lib
+{
+    public class DataService
+    {
+        //Пример линейной структуры
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        //Пример линейной структуры
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Пример линейной структуры
+        public static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        
+        public static double Division(int a, int b)
+        {
+            if (b == 0)
+            {
+                //Пример создан в целях демонстрации ветвления
+                //В реальных проектах нужно использовать Exception
+                Console.WriteLine("Переменная b = {0} на ноль делить нельзя", b);
+                return -1;
+            }
+            else
+            {
+                return a / b;
+            }
+        }
+    }
+}
---
+++ Tyuiu.PustobaevDO.Sprint0.Task5.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PustobaevDO.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PustobaevDO.Sprint0.Task5.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3b0888b7-bcf7-496d-a508-a03066eb2976")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
