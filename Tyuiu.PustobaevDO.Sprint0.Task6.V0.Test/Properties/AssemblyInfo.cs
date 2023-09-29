d4642c698a096ac585f6f9df8067d1459f803940
Tyuiu.PustobaevDO@outlook.com <Tyuiu.PustobaevDO@outlook.com>
2023-09-29 14:50:25 +0500

    Task6

---
+++ Tyuiu.PustobaevDO.Sprint0.Task6.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PustobaevDO.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PustobaevDO.Sprint0.Task6.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c5142ff1-3224-4738-a1ff-7b6b690f2707")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PustobaevDO.Sprint0.Task6.V0/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.PustobaevDO.Sprint0.Task6.V0.Lib;
+
+
+namespace Tyuiu.PustobaevDO.Sprint0.Task6.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numsArray = new int[] { 1, 2, 3, 4, 5 };
+
+            //Пример цикличесмкой структуры находится в библиотеке классов в методе AdditionArray
+            Console.WriteLine("Сумма элементов массива =" + DataService.AdditionArray(numsArray));
+
+            //Пример цикличесмкой структуры находится в библиотеке классов в методе SubtractionArray
+            Console.WriteLine("Разность элементов массива =" + DataService.SubstractionArray(numsArray));
+
+            //Пример цикличесмкой структуры находится в библиотеке классов в методе MultiplicationArray
+            Console.WriteLine("Произведение элементов массива =" + DataService.MultiplicationArray(numsArray));
+
+            Console.ReadKey();
+        }
+    }
+}
